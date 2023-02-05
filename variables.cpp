fe3ff2fb2975ad5f0cc42cae2994835a8e996d50
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-05 09:44:26 -0500

    practice with C++ variables

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() 
+{
+
+  // Declare a variable
+  int score;
+  int year;
+  
+  score = 1;
+  year = 1994;
+
+  std::cout << score;
+  std::cout << "\n";
+  std::cout << year;
+}

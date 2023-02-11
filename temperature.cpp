052dabdc616170f29371296a82e4c73d674f04e3
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-11 12:38:09 -0500

    practice with math and chaining output

---
+++ temperature.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  double tempf = 44;
+  double tempc;
+  tempc = (tempf - 32)/ 1.8;
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+  
+  
+  
+  
+}

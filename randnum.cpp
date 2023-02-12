0d3792d44b16b1410d159525d6e1642d8e84e01c
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-12 10:06:50 -0500

    practice with rand method

---
+++ randnum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+int main() {
+  
+  // This seeds the random number generator:
+  srand (time(NULL));
+  
+  // Use rand() below to initialize the_amazing_random_number
+  int the_amazing_random_number = rand() % 51;
+  std::cout << the_amazing_random_number << "\n";
+}
